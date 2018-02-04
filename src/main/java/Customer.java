82a7c63d6a13acc125b931e51693cf76ae0c51f5
vcorreia <valtergcorreia@ua.pt>
2018-02-04 23:37:12 +0000

    Overrided equals() method on Customer class

--- src/main/java/Customer.java
+++ src/main/java/Customer.java
@@ -4,15 +4,16 @@
  */
 
 import java.util.Comparator;
+import java.util.Objects;
 
 public class Customer{
 
     private String name;
     private int user_id;
-    private float longitude;
-    private float latitude;
+    private double longitude;
+    private double latitude;
 
-    public Customer(String name, int user_id, float longitude, float latitude) {
+    public Customer(String name, int user_id, double latitude, double longitude) {
         this.name = name;
         this.user_id = user_id;
         this.longitude = longitude;
@@ -40,21 +41,46 @@ public class Customer{
         this.user_id = user_id;
     }
 
-    public float getLongitude() {
+    public double getLongitude() {
         return longitude;
     }
 
-    public void setLongitude(float longitude) {
+    public void setLongitude(double longitude) {
         this.longitude = longitude;
     }
 
-    public float getLatitude() {
+    public double getLatitude() {
         return latitude;
     }
 
-    public void setLatitude(float latitude) {
+    public void setLatitude(double latitude) {
         this.latitude = latitude;
     }
 
+    @Override
+    public boolean equals(Object o){
 
+        //Checks if null
+        if (o == null){
+            return false;
+        }
+
+        //Checks agains itself
+        if (this == o){
+            return true;
+        }
+
+        //Checks if its the same type
+        if (this.getClass() != o.getClass()){
+            return false;
+        }
+
+        Customer anotherCustomer = (Customer) o;
+
+        //Compare customers information
+        return Objects.equals(this.name, anotherCustomer.getName())
+                && Objects.equals(this.user_id, anotherCustomer.getUser_id())
+                && Objects.equals(this.latitude, anotherCustomer.getLatitude())
+                && Objects.equals(this.longitude, anotherCustomer.getLongitude());
+    }
 }
